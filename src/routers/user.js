2e60ac60693a2b01b962060a11fd0238da79e05a
Suresh Gnanasekaran <skg@Sureshs-MBP.home>
2021-12-12 12:28:48 -0500

    Removed the mail sending

--- src/routers/user.js
+++ src/routers/user.js
@@ -24,7 +24,7 @@ router.post('/users', async (req, res)=> {
     const user = new User(req.body)
     try {
         await user.save()
-        sendWelcomeEmail(user.email, user.name)
+      //  sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
 
         res.status(201).send({user, token})
@@ -159,7 +159,7 @@ router.delete('/users/me', auth, async (req, res)=> {
     const _id = req.params.id;
 
     try {
-        sendCancellationEmail(user.email, user.name)
+     //   sendCancellationEmail(user.email, user.name)
        await  req.user.remove()
        res.status(200).send(req.user)
 
